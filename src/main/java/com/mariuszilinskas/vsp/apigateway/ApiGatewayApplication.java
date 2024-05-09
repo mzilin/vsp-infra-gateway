869ba03b7b489465e61bde307f4539c76cc67717
Marius Zilinskas <mariuzmail@gmail.com>
2024-05-09 19:47:36 +0100

    api gateway initiated

---
+++ src/main/java/com/mariuszilinskas/vsp/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.mariuszilinskas.vsp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@RefreshScope
+@EnableScheduling
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
