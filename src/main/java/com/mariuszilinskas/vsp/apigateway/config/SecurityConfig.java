4f9adaa17b7baa1a864bebd91f66272b80faced2
Marius Zilinskas <mariuzmail@gmail.com>
2024-05-28 16:46:38 +0100

    created security config

---
+++ src/main/java/com/mariuszilinskas/vsp/apigateway/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.mariuszilinskas.vsp.apigateway.config;
+
+import com.mariuszilinskas.vsp.apigateway.enums.UserRole;
+import com.mariuszilinskas.vsp.apigateway.util.AppUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorize -> authorize
+                        .pathMatchers(AppUtils.permitAllPaths.toArray(new String[0])).permitAll()
+                        .pathMatchers("/admin/**").hasAuthority(UserRole.ROLE_ADMIN.name())
+                        .anyExchange().authenticated()
+                )
+                .build();
+    }
+}
