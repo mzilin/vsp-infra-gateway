22a3dde519862f5466026a9c6ececc4ff17e01d8
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-05 03:20:20 +0100

    updated service error handler

--- src/main/java/com/mariuszilinskas/vsp/apigateway/handlers/AppExceptionHandler.java
+++ src/main/java/com/mariuszilinskas/vsp/apigateway/handlers/AppExceptionHandler.java
@@ -1,20 +1,13 @@
 package com.mariuszilinskas.vsp.apigateway.handlers;
 
 import com.mariuszilinskas.vsp.apigateway.dto.ErrorResponse;
-import com.mariuszilinskas.vsp.apigateway.dto.FieldErrorResponse;
 import com.mariuszilinskas.vsp.apigateway.exception.JwtTokenValidationException;
-import com.mariuszilinskas.vsp.apigateway.exception.NoAccessException;
-import com.mariuszilinskas.vsp.apigateway.exception.ResourceNotFoundException;
-import com.mariuszilinskas.vsp.apigateway.exception.SessionExpiredException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.*;
 
 /**
  * This class is a global exception handler that handles exceptions thrown across the whole application.
@@ -26,42 +19,9 @@ public class AppExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(AppExceptionHandler.class);
 
-    /**
-     * Exception for Request Validations
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<FieldErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-
-        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        FieldErrorResponse errorResponse = new FieldErrorResponse(errors, status.value(), status.getReasonPhrase());
-        return new ResponseEntity<>(errorResponse, status);
-    }
-
-    // --------------------- General ------------------------------
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        return buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    // --------------------- Specific -----------------------------
-
     @ExceptionHandler(JwtTokenValidationException.class)
     public ResponseEntity<ErrorResponse> handleJwtTokenValidationException(JwtTokenValidationException ex) {
         return buildErrorResponse(ex.getMessage(), HttpStatus.UNAUTHORIZED);
-    }
-
-    @ExceptionHandler(NoAccessException.class)
-    public ResponseEntity<ErrorResponse> handleNoAccessException(NoAccessException ex) {
-        return buildErrorResponse(ex.getMessage(), HttpStatus.FORBIDDEN);
-    }
-
-    @ExceptionHandler(SessionExpiredException.class)
-    public ResponseEntity<ErrorResponse> handleSessionExpiredException(SessionExpiredException ex) {
-        return buildErrorResponse(ex.getMessage(), HttpStatus.FORBIDDEN);
     }
 
     // -----------------------------------------------------------
