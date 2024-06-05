e8fd54dc9c2359fd3e841cfb83835c994c074c89
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-05 03:19:36 +0100

    updated service errors and handler

--- src/main/java/com/mariuszilinskas/vsp/apigateway/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.mariuszilinskas.vsp.apigateway.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(Class<?> entity, String type, Object value) {
-        super(entity.getSimpleName() + " with " + type + " '" + value + "' doesn't exist");
-    }
-}
