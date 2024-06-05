502ddda6f34e081ba35a96e84fcaec49b9ba6682
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-05 02:59:24 +0100

    created jwt service to validate jwt tokens

---
+++ src/main/java/com/mariuszilinskas/vsp/apigateway/service/JwtService.java
@@ -0,0 +1,12 @@
+package com.mariuszilinskas.vsp.apigateway.service;
+
+import com.mariuszilinskas.vsp.apigateway.dto.JwtPayload;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.Optional;
+
+public interface JwtService {
+
+    Optional<JwtPayload> verify(ServerWebExchange exchange);
+
+}
---
+++ src/main/java/com/mariuszilinskas/vsp/apigateway/service/JwtServiceImpl.java
@@ -0,0 +1,90 @@
+package com.mariuszilinskas.vsp.apigateway.service;
+
+import com.mariuszilinskas.vsp.apigateway.exception.JwtTokenValidationException;
+import com.mariuszilinskas.vsp.apigateway.dto.JwtPayload;
+import com.mariuszilinskas.vsp.apigateway.util.AppUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpCookie;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerWebExchange;
+
+import javax.crypto.SecretKey;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Service implementation for managing JWT tokens.
+ *
+ * @author Marius Zilinskas
+ */
+@Service
+@RequiredArgsConstructor
+public class JwtServiceImpl implements JwtService {
+
+    @Value("${app.accessTokenSecret}")
+    private String accessTokenSecret;
+
+    public Optional<JwtPayload> verify(ServerWebExchange exchange) {
+        String token = extractAccessToken(exchange);
+
+        if (token != null && !token.isBlank()) {
+            Claims claims = extractClaims(token);
+
+            JwtPayload payload = new JwtPayload(
+                    claims.getSubject(),
+                    safelyExtractListFromClaims(claims, "roles"),
+                    safelyExtractListFromClaims(claims, "authorities"),
+                    claims.getExpiration()
+            );
+
+            return Optional.of(payload);
+        }
+
+        return Optional.empty();
+    }
+
+    private String extractAccessToken(ServerWebExchange exchange) {
+        return exchange.getRequest()
+                .getCookies()
+                .getOrDefault(AppUtils.ACCESS_TOKEN_NAME, Collections.emptyList())
+                .stream()
+                .findFirst()
+                .map(HttpCookie::getValue)
+                .orElse(null);
+    }
+
+    private List<String> safelyExtractListFromClaims(Map<String, Object> claims, String claimKey) {
+        Object claimValue = claims.get(claimKey);
+        if (claimValue instanceof List<?> rawList) {
+            return rawList.stream()
+                    .filter(obj -> obj instanceof String)
+                    .map(obj -> (String) obj)
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+
+    private Claims extractClaims(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(getAccessTokenSecret())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (JwtException ex) {
+            System.out.println(ex.getMessage());
+            throw new JwtTokenValidationException();
+        }
+    }
+
+    private SecretKey getAccessTokenSecret() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(accessTokenSecret));
+    }
+
+}
